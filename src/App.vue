c3b55a70d26d58116336cfbb4748dd98ae443c89
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-10 00:59:18 +0300

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="app">
-    <h1>Hello World</h1>
+    <navbar />
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar";
 export default {
   name: "app",
-  components: {}
+  components: { Navbar }
 };
 </script>
 
