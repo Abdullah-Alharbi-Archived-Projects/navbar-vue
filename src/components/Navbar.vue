f7ad92478b8c4d888d4ea655a5799274ee3513aa
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-10 02:47:16 +0300

    impl: links prop

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,45 @@
 <template>
   <nav>
     <!-- dynamic component -->
-    <component v-if="typeof config.brand == 'function'" :is="config.brand()" />
+    <component v-if="brandIsFunction" :is="config.brand()" />
     <header v-else>
       <!-- set string in h3 -->
       <h3>{{ config.brand }}</h3>
     </header>
+
+    <ul
+      v-if="canRenderLinks && !renderRouterLinks && !renderCustomLinks"
+      :class="renderCustomClass(config.ulClass, 'nav-links')"
+    >
+      <li v-for="link in config.links" :key="link.to" :class="renderCustomClass(config.liClass)">
+        <a :href="link.to" :class="renderCustomClass(config.aClass)" v-if="link.icon">
+          <component :is="link.icon()" />
+          {{ link.value }}
+        </a>
+        <a :href="link.to" :class="renderCustomClass(config.aClass)" v-else>{{ link.value }}</a>
+      </li>
+    </ul>
+    <ul
+      v-else-if="canRenderLinks && renderRouterLinks"
+      :class="renderCustomClass(config.ulClass, 'nav-links')"
+    >
+      <li v-for="link in config.links" :key="link.to" :class="renderCustomClass(config.liClass)">
+        <router-link :to="link.to" :class="renderCustomClass(config.liClass)" v-if="link.icon">
+          <component :is="link.icon()" />
+          {{ link.value }}
+        </router-link>
+        <router-link
+          :to="link.to"
+          :class="renderCustomClass(config.liClass)"
+          v-else
+        >{{ link.value }}</router-link>
+      </li>
+    </ul>
+    <component
+      v-else-if="canRenderLinks && renderCustomLinks"
+      :is="config.customLinksComponent()"
+      :links="config.links"
+    />
   </nav>
 </template>
 
@@ -15,6 +49,28 @@ export default {
   props: {
     config: {
       type: Object
+    }
+  },
+  methods: {
+    renderCustomClass(customClass, defaultClass = undefined) {
+      return customClass ? customClass : defaultClass;
+    }
+  },
+  computed: {
+    renderCustomLinks() {
+      return typeof this.config.customLinksComponent === "function";
+    },
+    renderRouterLinks() {
+      return this.config.useAnchor === false;
+    },
+    canRenderLinks() {
+      return typeof this.config.links === "object";
+    },
+    useAnchorIsBoolean() {
+      return typeof this.config.useAnchor === "boolean";
+    },
+    brandIsFunction() {
+      return typeof this.config.brand == "function";
     }
   }
 };
