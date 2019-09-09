ee85f816dff34fed389551f59893f4e45b306e1d
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-10 00:58:57 +0300

    initial setup

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <div class="menu-icon">
+      <div class="line"></div>
+      <div class="line"></div>
+      <div class="line"></div>
+    </div>
+
+    <ul>
+      <li>
+        <a href="#">Link</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "navbar",
+  props: {
+    config: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
