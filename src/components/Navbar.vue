cec2fc5d964af38ba3e2022e60bf8424393586ee
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-10 04:04:13 +0300

    added computed props plus helper methods

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,43 +1,68 @@
 <template>
-  <nav>
-    <component v-if="brandIsFunction" :is="config.brand()" />
-    <header v-else>
-      <h3>{{ config.brand }}</h3>
-    </header>
+  <nav :class="renderCustomClass(config.ulClass, 'navbar')">
+    <div class="hamburger" @click="handleMenuClick">
+      <div class="line"></div>
+      <div class="line"></div>
+      <div class="line"></div>
+    </div>
+    <div class="children">
+      <component v-if="brandIsFunction" :is="config.brand()" />
+      <div class="position" v-else>
+        <header class="header-w">
+          <a
+            href="/"
+            :class="renderCustomClass(config.brandClass, 'brand-link')"
+            v-if="!renderRouterLinks && !renderCustomLinks"
+          >
+            <h3>{{ config.brand }}</h3>
+          </a>
+          <router-link
+            to="/"
+            :class="renderCustomClass(config.brandClass, 'brand-link')"
+            v-else-if="renderRouterLinks"
+          >
+            <h3>{{ config.brand }}</h3>
+          </router-link>
+        </header>
+      </div>
 
-    <ul
-      v-if="canRenderLinks && !renderRouterLinks && !renderCustomLinks"
-      :class="renderCustomClass(config.ulClass, 'nav-links')"
-    >
-      <li v-for="link in config.links" :key="link.to" :class="renderCustomClass(config.liClass)">
-        <a :href="link.to" :class="renderCustomClass(config.aClass)" v-if="link.icon">
-          <component :is="link.icon()" />
-          {{ link.value }}
-        </a>
-        <a :href="link.to" :class="renderCustomClass(config.aClass)" v-else>{{ link.value }}</a>
-      </li>
-    </ul>
-    <ul
-      v-else-if="canRenderLinks && renderRouterLinks"
-      :class="renderCustomClass(config.ulClass, 'nav-links')"
-    >
-      <li v-for="link in config.links" :key="link.to" :class="renderCustomClass(config.liClass)">
-        <router-link :to="link.to" :class="renderCustomClass(config.liClass)" v-if="link.icon">
-          <component :is="link.icon()" />
-          {{ link.value }}
-        </router-link>
-        <router-link
-          :to="link.to"
-          :class="renderCustomClass(config.liClass)"
-          v-else
-        >{{ link.value }}</router-link>
-      </li>
-    </ul>
-    <component
-      v-else-if="canRenderLinks && renderCustomLinks"
-      :is="config.customLinksComponent()"
-      :links="config.links"
-    />
+      <div class="hamburger" @click="handleMenuClick" v-if="false">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+      </div>
+
+      <ul
+        v-if="canRenderLinks && !renderRouterLinks && !renderCustomLinks"
+        :class="generateOpenClass"
+      >
+        <li v-for="link in config.links" :key="link.to" :class="renderCustomClass(config.liClass)">
+          <a :href="link.to" :class="renderCustomClass(config.aClass)" v-if="link.icon">
+            <component :is="link.icon()" />
+            {{ link.value }}
+          </a>
+          <a :href="link.to" :class="renderCustomClass(config.aClass)" v-else>{{ link.value }}</a>
+        </li>
+      </ul>
+      <ul v-else-if="canRenderLinks && renderRouterLinks" :class="generateOpenClass">
+        <li v-for="link in config.links" :key="link.to" :class="renderCustomClass(config.liClass)">
+          <router-link :to="link.to" :class="renderCustomClass(config.liClass)" v-if="link.icon">
+            <component :is="link.icon()" />
+            {{ link.value }}
+          </router-link>
+          <router-link
+            :to="link.to"
+            :class="renderCustomClass(config.liClass)"
+            v-else
+          >{{ link.value }}</router-link>
+        </li>
+      </ul>
+      <component
+        v-else-if="canRenderLinks && renderCustomLinks"
+        :is="config.customLinksComponent()"
+        :links="config.links"
+      />
+    </div>
   </nav>
 </template>
 
@@ -52,6 +77,12 @@ export default {
   methods: {
     renderCustomClass(customClass, defaultClass = undefined) {
       return customClass ? customClass : defaultClass;
+    },
+    navbarIsOpen() {
+      return this.config.mobileMode ? "open" : undefined;
+    },
+    handleMenuClick() {
+      this.$emit("menu-click");
     }
   },
   computed: {
@@ -60,6 +91,14 @@ export default {
     },
     renderRouterLinks() {
       return this.config.useAnchor === false;
+    },
+    generateOpenClass() {
+      const defaultClass = this.renderCustomClass(
+        this.config.ulClass,
+        "nav-links"
+      );
+      const navbarIsOpen = this.navbarIsOpen() ? " " + this.navbarIsOpen() : "";
+      return defaultClass + navbarIsOpen;
     },
     canRenderLinks() {
       return typeof this.config.links === "object";
